5040162b865cfa81c1e4b42e3e778abc1393237a
arbenoruci-pixel <arbenoruci@gmail.com>
2026-08-19 18:07:43 -0400

    show daily overview and close shortcut throughout ARKA

--- app/arka/layout.jsx
+++ app/arka/layout.jsx
@@ -1,9 +1,13 @@
 import './arka.css';
+import ArkaDailyCloseShortcut from '@/components/ArkaDailyCloseShortcut.jsx';
 
 export default function ArkaLayout({ children }) {
   return (
     <div className="arka-shell">
-      <div className="arka-container">{children}</div>
+      <div className="arka-container">
+        {children}
+        <ArkaDailyCloseShortcut />
+      </div>
     </div>
   );
 }
